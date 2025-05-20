33756da758dd7a390cbddb7bf8de379fac722ea1
guyt6517 <guyt6516@gmail.com>
2025-05-20 12:07:13 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from flask_app.py import app  # replace 'your_flask_file' with the name of your Python file (without .py)
+from flask_app import app  # replace 'your_flask_file' with the name of your Python file (without .py)
 
 # WSGI callable
 application = app

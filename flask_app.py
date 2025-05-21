1ffeecc57bb7779a0afed158c4a5ee8ea1b40dcb
guyt6517 <guyt6516@gmail.com>
2025-05-21 08:51:26 -0500

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -40,7 +40,9 @@ def proxy():
         # Add CORS header
         headers.append(('Access-Control-Allow-Origin', '*'))
         headers.append(('Content-Type', content_type))
-
+        headers.append(('x-frame-options', '*'))
+        headers.append(('content-security-policy', '*'))
+        headers.append(('cross-origin-embedder-policy', '*'))
         return Response(content, resp.status_code, headers)
     except Exception as e:
         return str(e), 500

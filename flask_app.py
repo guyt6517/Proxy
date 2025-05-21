dcc607166d7f903f8af42e63f32f3d45239a3393
guyt6517 <guyt6516@gmail.com>
2025-05-21 10:05:38 -0500

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, Response
 import requests
-from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -8,30 +7,8 @@ app = Flask(__name__)
 def proxy():
     target_url = request.args.get('url')
     if not target_url:
-        return "Missing url parameter", 400
-
-    try:
-        # Fetch the content
-        resp = requests.get(target_url)
-        content_type = resp.headers.get('Content-Type', '')
-
-        content = resp.content
-
-        # If it's HTML, parse and strip <script> and <meta http-equiv="refresh">
-        if 'text/html' in content_type:
-            soup = BeautifulSoup(content, 'html.parser')
-
-            # Remove all <script> tags
-            for script in soup.find_all('script'):
-                script.decompose()
-
-            # Remove <meta http-equiv="refresh">
-            for meta in soup.find_all('meta', attrs={'http-equiv': True}):
-                if meta['http-equiv'].lower() == 'refresh':
-                    meta.decompose()
-
-            content = str(soup).encode('utf-8')
-
+        return "Missing url parameter", 
+    if True:
         # Build response
         excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
         headers = [(name, value) for (name, value) in resp.raw.headers.items()
@@ -46,4 +23,3 @@ def proxy():
         return Response(content, resp.status_code, headers)
     except Exception as e:
         return str(e), 500
-@app.route('/ip')
